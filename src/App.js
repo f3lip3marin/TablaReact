b97d697596bbe4e36f592acc8a1cb9dc799acef4
Andres Felipe Ramirez <87092857+f3lip3marin@users.noreply.github.com>
2022-05-20 19:43:29 -0500

    Commit final

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import logo from './imagenes/logotipo.png';
+import ListaDeTareas from './componentes/ListaDeTareas';
+
+
+ function App() {
+  return (
+    <div className='aplicacion-tareas'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          src={logo} 
+          className='freecodecamp-logo' />
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas />
+      </div>
+    </div>
+  );
+}
+
+export default App;
